e92998667d8da460e69b079c2c51ad8cc222ca35
fimion <fimion@gmail.com>
2022-08-23 18:36:24 -0400

    move to use a vue sfc

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 // import React from 'react'
 // import ReactDOM from 'react-dom/client'
 import {createApp} from "vue"
-import App from './App'
+import App from './App.vue'
 import './index.css'
 
 // ReactDOM.createRoot(document.getElementById('root')).render(
